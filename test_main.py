5859fe9720f9e8ab2a5d7bfeb5086c4c4f332d0e
Nguyen Ba Tuyen <batuyen0510@gmail.com>
2024-08-07 16:59:43 +0700

    loi

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import add
 
 class TestMain(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), -1)
 
 if __name__ == "__main__":
     unittest.main()
